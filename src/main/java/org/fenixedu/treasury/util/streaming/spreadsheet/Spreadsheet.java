1eaef458d2b6c96257fc57dacd1fa9785c85414b
Anil Mamede <anil.mamede@qub-it.com>
2020-09-07 11:18:28 +0100

    - Improvement: Improve spreasheet generation to use less memory

--- src/main/java/org/fenixedu/treasury/util/streaming/spreadsheet/Spreadsheet.java
+++ src/main/java/org/fenixedu/treasury/util/streaming/spreadsheet/Spreadsheet.java
@@ -1,8 +1,13 @@
 package org.fenixedu.treasury.util.streaming.spreadsheet;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
@@ -16,6 +21,23 @@ public interface Spreadsheet {
     public ExcelSheet[] getSheets();
     
     public static byte[] buildSpreadsheetContent(final Spreadsheet spreadsheet, final IErrorsLog errorsLog) {
+        
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        try {
+            buildSpreadsheetContent(spreadsheet, errorsLog, output);
+        } finally {
+            try {
+                output.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        
+        return output.toByteArray();
+    }
+
+    public static void buildSpreadsheetContent(final Spreadsheet spreadsheet, final IErrorsLog errorsLog, OutputStream outputStream) {
         final SXSSFWorkbook wb = new SXSSFWorkbook(ROWS_IN_MEMORY);
         
         for (ExcelSheet sheet : spreadsheet.getSheets()) {
@@ -41,23 +63,20 @@ public interface Spreadsheet {
             sheet.getRows().forEach(r -> r.writeCellValues(sh.createRow(sh.getLastRowNum() + 1), errorsLog));
         }
         
-        
-        final ByteArrayOutputStream output = new ByteArrayOutputStream();
-        
         try {
-            wb.write(output);
+            File tempFile = File.createTempFile("treasury-spreadsheet" + wb.hashCode() + "-" + System.currentTimeMillis(), "xlsx");
+            
+            try(FileOutputStream output = new FileOutputStream(tempFile)) {
+                wb.write(output);
+            }
+                
+            try(FileInputStream fileInputStream = new FileInputStream(tempFile)) {
+                IOUtils.copy(fileInputStream, outputStream);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
-            try {
-                output.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         }
-        
-        return output.toByteArray();
     }
-    
-    
+
 }
