8c11f6051d8d0c70b013e9d07cdbab484d02e38d
Diogo Godinho <diogo.godinho@qub-it.com>
2021-04-06 08:53:02 +0100

    Refatorization of payment types interfaces

--- src/main/java/org/fenixedu/treasury/domain/forwardpayments/implementations/IForwardPaymentPlatformService.java
+++ src/main/java/org/fenixedu/treasury/domain/forwardpayments/implementations/IForwardPaymentPlatformService.java
@@ -54,31 +54,46 @@ package org.fenixedu.treasury.domain.forwardpayments.implementations;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 import org.apache.commons.lang.StringUtils;
+import org.fenixedu.onlinepaymentsgateway.api.DigitalPlatformResultBean;
 import org.fenixedu.treasury.domain.forwardpayments.ForwardPaymentRequest;
-import org.fenixedu.treasury.domain.payments.PaymentRequest;
+import org.fenixedu.treasury.dto.SettlementNoteBean;
 import org.fenixedu.treasury.dto.forwardpayments.ForwardPaymentStatusBean;
 
 public interface IForwardPaymentPlatformService {
 
-    public IForwardPaymentController getForwardPaymentController(ForwardPaymentRequest request);
+    public ForwardPaymentRequest createForwardPaymentRequest(SettlementNoteBean bean,
+            Function<ForwardPaymentRequest, String> successUrlFunction,
+            Function<ForwardPaymentRequest, String> insuccessUrlFunction);
+
+    public default IForwardPaymentController getForwardPaymentController(ForwardPaymentRequest request) {
+        return IForwardPaymentController.getForwardPaymentController(request);
+    }
 
     public String getPaymentURL(ForwardPaymentRequest request);
 
     default public boolean isLogosPageDefined() {
         return !StringUtils.isEmpty(getLogosJspPage());
     }
-    
+
     public String getLogosJspPage();
 
-    public String getWarningBeforeRedirectionJspPage();
+    public default String getWarningBeforeRedirectionJspPage() {
+        return null;
+    }
 
     public ForwardPaymentStatusBean paymentStatus(ForwardPaymentRequest request);
 
     public PostProcessPaymentStatusBean postProcessPayment(ForwardPaymentRequest forwardPayment, String justification,
             Optional<String> specificTransactionId);
 
-    public List<ForwardPaymentStatusBean> verifyPaymentStatus(final ForwardPaymentRequest request);
+    public List<? extends DigitalPlatformResultBean> getPaymentTransactionsReportListByMerchantId(String merchantTransationId);
+
+    public PostProcessPaymentStatusBean processForwardPayment(ForwardPaymentRequest forwardPayment);
+
+//    public ForwardPaymentStatusBean prepareCheckout(ForwardPaymentRequest forwardPayment, SibsBillingAddressBean addressBean);
+//    public List<ForwardPaymentStatusBean> verifyPaymentStatus(final ForwardPaymentRequest request);
 
 }
