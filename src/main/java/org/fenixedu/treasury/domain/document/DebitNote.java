56a46f9dffa242deabf2d30a2d4c40d00cd83f13
Diogo Godinho <diogo.godinho@qub-it.com>
2021-12-07 10:15:22 +0000

    - Improvement: When a debit note is annulled, then the interests debit entries are annulled too

--- src/main/java/org/fenixedu/treasury/domain/document/DebitNote.java
+++ src/main/java/org/fenixedu/treasury/domain/document/DebitNote.java
@@ -388,7 +388,17 @@ public class DebitNote extends DebitNote_Base {
         if (this.getFinantialDocumentEntriesSet().size() > 0 && this.isClosed()) {
 
             final DateTime now = new DateTime();
-
+            
+            //Annul open interest debit entry
+            getDebitEntries().flatMap(entry -> entry.getInterestDebitEntriesSet().stream())
+                    .filter(interest -> interest.getFinantialDocument() == null || interest.getFinantialDocument().isPreparing())
+                    .forEach(interest -> {
+                         if(interest.getFinantialDocument() == null ) {
+                            interest.annulDebitEntry(reason);
+                         }else {
+                             interest.getDebitNote().anullDebitNoteWithCreditNote(reason, false);
+                         }
+                    });
             //1. criar nota de acerto
             //2. percorrer os itens de divida, criar correspondente item de acerto com o valor "aberto"
             //2.1 verificar se existiram "juros" gerados correspondentes
@@ -493,6 +503,10 @@ public class DebitNote extends DebitNote_Base {
 
         for (final DebitEntry debitEntry : this.getDebitEntriesSet()) {
             for (DebitEntry interestEntry : debitEntry.getInterestDebitEntriesSet()) {
+                if(!interestEntry.getFinantialDocument().isClosed()) {
+                    continue;
+                }
+                
                 final BigDecimal amountForCreditWithoutVat = interestEntry.getCurrency().getValueWithScale(TreasuryConstants
                         .divide(interestEntry.getAvailableAmountForCredit(), BigDecimal.ONE.add(rationalVatRate(interestEntry))));
 
