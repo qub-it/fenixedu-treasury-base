be0d54eadbbe6a3320632eaabf6cd836d21c72a9
Anil Mamede <anil.mamede@qub-it.com>
2026-07-06 18:06:15 +0100

    - Refactor: Servlet to forward requests of old webhook Spring controller to new JAX RS controller [#qubIT-Fenix-8910]

---
+++ src/main/java/org/fenixedu/treasury/servlet/payments/sibspaymentsgateway/SibsPaymentsGatewayWebhookForwardServlet.java
@@ -0,0 +1,58 @@
+package org.fenixedu.treasury.servlet.payments.sibspaymentsgateway;
+
+import com.qubit.terra.framework.services.ServiceProvider;
+import com.qubit.terra.framework.services.logging.Log;
+import com.qubit.terra.framework.services.logging.LogContext;
+import org.fenixedu.treasury.domain.sibsonlinepaymentsgateway.SibsOnlinePaymentsGatewayEnviromentMode;
+import org.fenixedu.treasury.domain.sibspaymentsgateway.integration.SibsPaymentsGateway;
+import pt.ist.fenixframework.FenixFramework;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet("/treasury/document/payments/onlinepaymentsgateway/notification")
+public class SibsPaymentsGatewayWebhookForwardServlet extends HttpServlet {
+
+    private static LogContext LOG_CONTEXT = LogContext.forContext(SibsPaymentsGatewayWebhookForwardServlet.class.getSimpleName());
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        boolean forwardToJaxRsSibsPaymentsGatewayWebhookController = checkForwardToJaxRsSibsPaymentsGatewayWebhookController();
+
+        String target =
+                forwardToJaxRsSibsPaymentsGatewayWebhookController ? "/api/sibspaymentsgatewaywebhook" : "/treasury/document/payments/onlinepaymentsgateway/deprecated/notification";
+
+        forwardToWebhookController(req, resp, target);
+    }
+
+    private boolean checkForwardToJaxRsSibsPaymentsGatewayWebhookController() {
+        Optional<SibsPaymentsGateway> gateway = SibsPaymentsGateway.findAll().filter(g -> g.isActive()).findFirst();
+
+        if (gateway.isPresent()) {
+            return Boolean.TRUE.equals(gateway.get().getForwardWebhooksToNewController());
+        }
+
+        return false;
+    }
+
+    private void forwardToWebhookController(HttpServletRequest req, HttpServletResponse resp, String target)
+            throws ServletException, IOException {
+        Log.info(LOG_CONTEXT, "Forwarding SIBS notification to %s".formatted(target));
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher(target);
+
+        // optional: keep original query string
+        if (req.getQueryString() != null && !req.getQueryString().isEmpty()) {
+            target = target + "?" + req.getQueryString();
+        }
+
+        dispatcher.forward(req, resp);
+    }
+
+}
